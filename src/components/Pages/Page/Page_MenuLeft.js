a7030c2162fb622a8d859943ea639ebb20da84af
DESKTOP-7104OK7\Nick <nicktabolich@gmail.com>
2018-04-12 13:10:14 +0300

    menu / @baseHeight / Dashboard.css name fix

--- src/components/Pages/Page/Page_MenuLeft.js
+++ src/components/Pages/Page/Page_MenuLeft.js
@@ -3,8 +3,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { toggleMenuLeft } from 'ducks/app'
 
-import { Menu, Icon, Switch } from 'antd';
-const SubMenu = Menu.SubMenu;
+import { Menu, Icon, Switch } from 'antd'
+const SubMenu = Menu.SubMenu
 
 const mapStateToProps = (state, props) => ({})
 
@@ -25,16 +25,16 @@ class MenuLeft extends React.Component {
     theme: 'dark',
     current: '1',
   }
-  changeTheme = (value) => {
+  changeTheme = value => {
     this.setState({
       theme: value ? 'dark' : 'light',
-    });
+    })
   }
-  handleClick = (e) => {
-    console.log('click ', e);
+  handleClick = e => {
+    console.log('click ', e)
     this.setState({
       current: e.key,
-    });
+    })
   }
 
   render() {
@@ -71,13 +71,29 @@ class MenuLeft extends React.Component {
               selectedKeys={[this.state.current]}
               mode="inline"
             >
-              <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="mail" />
+                    <span>Navigation One</span>
+                  </span>
+                }
+              >
                 <Menu.Item key="1">Option 1</Menu.Item>
                 <Menu.Item key="2">Option 2</Menu.Item>
                 <Menu.Item key="3">Option 3</Menu.Item>
                 <Menu.Item key="4">Option 4</Menu.Item>
               </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigtion Two</span></span>}>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="appstore" />
+                    <span>Navigtion Two</span>
+                  </span>
+                }
+              >
                 <Menu.Item key="5">Option 5</Menu.Item>
                 <Menu.Item key="6">Option 6</Menu.Item>
                 <SubMenu key="sub3" title="Submenu">
@@ -85,7 +101,15 @@ class MenuLeft extends React.Component {
                   <Menu.Item key="8">Option 8</Menu.Item>
                 </SubMenu>
               </SubMenu>
-              <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
+              <SubMenu
+                key="sub4"
+                title={
+                  <span>
+                    <Icon type="setting" />
+                    <span>Navigation Three</span>
+                  </span>
+                }
+              >
                 <Menu.Item key="9">Option 9</Menu.Item>
                 <Menu.Item key="10">Option 10</Menu.Item>
                 <Menu.Item key="11">Option 11</Menu.Item>
