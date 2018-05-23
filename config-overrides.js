f7232af1672e9aa4a78f2072b0cd434a019705e5
nicktabolich <nicktabolich@gmail.com>
2018-05-23 14:40:36 +0300

    packages cleaning / rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,23 @@
 const rewired = require('react-app-rewired')
-const { injectBabelPlugin } = require('react-app-rewired')
 const rewireLess = require('react-app-rewire-less')
 const rewireEslint = require('react-app-rewire-eslint')
 
 function rewire(config, env) {
-  config = injectBabelPlugin('transform-decorators-legacy', config)
-
   const cssLoader = rewired.getLoader(
     config.module.rules,
     rule => rule.test && String(rule.test) === String(/\.css$/)
   )
-
   const sassLoader = {
     test: /\.scss$/,
     use: [...(cssLoader.loader || cssLoader.use), 'sass-loader']
   }
-
   const oneOf = config.module.rules.find(rule => rule.oneOf).oneOf
   oneOf.unshift(sassLoader)
 
+  config = rewired.injectBabelPlugin('transform-decorators-legacy', config)
   config = rewireLess(config, env)
   config = rewireEslint(config, env)
+
   return config
 }
 
